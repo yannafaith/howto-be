190140cb83a59fd48d3c78e6049daed7ca53e84f
yannafaith <yannafaith97@gmail.com>
2019-03-14 14:30:03 -0700

    updates config vars due to heroku maintanence

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
-const environment = process.env.ENVIRONMENT || 'development';
+const environment = process.env.ENVIRONMENT || 'production';
 const db =  knex(knexConfig[environment])
 
 module.exports = db;
