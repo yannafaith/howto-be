21e5c7d2b30fe5e2b4aaecda568d99028da1c68d
yannafaith <yannafaith97@gmail.com>
2019-03-14 17:03:22 -0700

    adds cors

--- api/server.js
+++ api/server.js
@@ -11,7 +11,12 @@ const posts_categories = require('./routes/posts_categories_router.js')
 
 server.use(express.json());
 server.use(helmet());
-server.use(cors());
+server.use(
+    cors({
+      origin: ['http://localhost:3000', 'https://howto-frontend.netlify.com'],
+      credentials: true,
+    }),
+  );
 
 server.use('/api/users', users);
 server.use('/api/auth', auth);
