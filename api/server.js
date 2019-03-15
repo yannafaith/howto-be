a4ef87a98ba7f7fe60cfbd6a60565510ec074848
yannafaith <yannafaith97@gmail.com>
2019-03-14 18:14:01 -0700

    refac

--- api/server.js
+++ api/server.js
@@ -40,7 +40,7 @@ server.use(cors({
 
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://howto-frontend.netlify.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header('Access-Control-Allow-Methods', "GET, PUT, POST, DELETE, PATCH, OPTIONS");
     res.header("Access-Control-Allow-Credentials", true);
     if (req.method === "OPTIONS") {
